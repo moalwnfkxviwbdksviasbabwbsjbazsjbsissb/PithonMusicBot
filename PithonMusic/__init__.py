07c028f0282a5751a1c2e742bf4367515c98f75c
azooziraqi <62655481+azooziraqi@users.noreply.github.com>
2023-07-19 02:19:54 +0300

    first commit

---
+++ PithonMusic/__init__.py
@@ -0,0 +1,38 @@
+from PithonMusic.core.bot import PithonBot
+from PithonMusic.core.dir import dirr
+from PithonMusic.core.git import git
+from PithonMusic.core.userbot import Userbot
+from PithonMusic.misc import dbb, heroku, sudo
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = PithonBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
